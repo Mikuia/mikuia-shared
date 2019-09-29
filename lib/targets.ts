b4cfe396653e6097db562ec9a3b1065eb34f8c5f
Laura Lynn <laura@iys.io>
2019-09-29 19:55:14 +0200

    moving auth checks to shareed

--- lib/targets.ts
+++ lib/targets.ts
@@ -1,8 +1,26 @@
-import {Target} from './target';
 import {PromisifiedRedisClient} from './typings/redis';
 
-export class Targets {	
-	constructor(private db: PromisifiedRedisClient) {}
+import {Target} from './target';
+import {Users} from './users';
+
+export class Targets {
+	private users: Users;
+
+	constructor(private db: PromisifiedRedisClient) {
+		this.users = new Users(db);
+	}
+
+	async checkAuth(userId: string, targetService: string, targetServiceId: string): Promise<boolean> {
+		var serviceId = await this.users.getServiceIdByUserId(userId, targetService);
+		if(!serviceId) return false;
+
+		if(targetServiceId === serviceId) return true;
+
+		var isAuthorized = await this.db.sismemberAsync(`target:${targetService}:${targetServiceId}:permissions`, serviceId);
+		if(isAuthorized) return true;
+
+		return false;
+	}
 
 	getByServiceId(service: string, serviceId: string): Target {
 		return new Target(service, serviceId, this.db);
